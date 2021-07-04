08ae4ab0b582d171d09981aef62ff68d8f719a4b
counter0021 <ara123rus123ara@gmail.com>
2021-07-04 19:55:17 +0300

    Warnings

--- bot.py
+++ bot.py
@@ -4,12 +4,51 @@ import logging
 
 from config import TOKEN, GROUP_ID
 from filters import IsAdminFilter
+from sqlighter import SQLighter
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TOKEN, parse_mode='HTML')
 dispatcher = Dispatcher(bot)
 dispatcher.filters_factory.bind(IsAdminFilter)
+db = SQLighter('members.db')
+
+
+# New warning
+@dispatcher.message_handler(commands=['warning'], is_admin=True)
+async def add_new_warning(message: types.Message):
+
+    if not message.reply_to_message:
+        await message.reply('Command need use with reply message!')
+        return
+
+    user_may_be_is_admin = await message.bot.get_chat_member(GROUP_ID, message.reply_to_message.from_user.id)
+    if user_may_be_is_admin.is_chat_admin():
+        await message.reply('Error give warning admin!')
+        return
+
+    user = message.reply_to_message.from_user
+    user_id = user.id
+    db.add_new_warning(user_id)
+    count_warning = db.get_count_warnings(user_id)
+    await message.bot.delete_message(GROUP_ID, message.message_id)
+
+    if count_warning >= 3:
+        await bot.send_message(GROUP_ID, f'We warned you! @{user.username}')
+        await message.bot.kick_chat_member(GROUP_ID, user_id, revoke_messages=True)
+        await bot.send_message(GROUP_ID, f'User @{user.username} has been banned!')
+        await message.bot.send_photo(message.chat.id, 'https://airsofter.world/galleries/3857/5cb720e8783a1.jpg')
+    else:
+        await bot.send_message(
+            GROUP_ID, f'You have {count_warning} warnings! If you have 3 warnings we ban you! @{user.username}',
+        )
+
+
+# New add link
+@dispatcher.message_handler(commands=['link'])
+async def get_add_link(message: types.Message):
+    link = await message.bot.create_chat_invite_link(message.chat.id)
+    await message.answer(link['invite_link'])
 
 
 # Ban
@@ -21,13 +60,13 @@ async def ban(message: types.Message):
 
     user = await message.bot.get_chat_member(GROUP_ID, message.reply_to_message.from_user.id)
     if user.is_chat_admin():
-        await message.reply('Error ban admin')
+        await message.reply('Error ban admin!')
         return
 
     await message.bot.delete_message(GROUP_ID, message.message_id)
     await message.bot.kick_chat_member(GROUP_ID, message.reply_to_message.from_user.id, revoke_messages=True)
 
-    await message.reply_to_message.reply('User has been banned!')
+    await message.reply_to_message.reply(f'User @{message.reply_to_message.from_user.username} has been banned!')
     await message.bot.send_photo(message.chat.id, 'https://airsofter.world/galleries/3857/5cb720e8783a1.jpg')
 
 
@@ -36,6 +75,10 @@ async def ban(message: types.Message):
     content_types=['new_chat_members', 'left_chat_member', 'new_chat_title', 'new_chat_photo']
 )
 async def on_user_joined(message: types.Message):
+    if message.content_type == 'new_chat_members':
+        for member in message.new_chat_members:
+            db.add_user(member['id'])
+            db.add_new_warning(member['id'], 0)
     await message.delete()
 
 
---
+++ sqlighter.py
@@ -0,0 +1,68 @@
+import logging
+
+import sqlite3
+
+logger = logging.getLogger('SQLite')
+
+
+class SQLighter:
+
+    def __init__(self, database):
+        self.connection = sqlite3.connect(database)
+        self.cursor = self.connection.cursor()
+        self.create_table()
+
+    def create_table(self):
+        table = """
+                    CREATE TABLE IF NOT EXISTS warnings (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        user_id VARCHAR(255) NOT NULL UNIQUE,
+                        count_warnings INTEGER NOT NULL DEFAULT 0
+                    );
+                """
+
+        with self.connection:
+            self.cursor.execute(table)
+            logger.info('Table created')
+
+    def exists(self, user_id: int):
+
+        with self.connection:
+            user_exists = bool(
+                len(self.cursor.execute("SELECT * FROM warnings WHERE user_id = ?", (user_id,)).fetchall()))
+            logger.info(f"User exists = {user_exists}")
+            return user_exists
+
+    def add_user(self, user_id: int):
+
+        with self.connection:
+            logger.info(f'Add new user ({user_id})')
+            if not self.exists(user_id):
+                return self.cursor.execute("INSERT INTO warnings (user_id) VALUES (?);", (user_id,))
+            return
+
+    def get_count_warnings(self, user_id: int):
+
+        with self.connection:
+            return int(
+                self.cursor.execute(
+                    "SELECT count_warnings FROM warnings WHERE user_id = (?);", (user_id,)
+                ).fetchone()[0]
+            )
+
+    def add_new_warning(self, user_id: int, set_warnings=None):
+
+        with self.connection:
+            logger.info(f'Add new warning for user with id {user_id}')
+            count_warnings = self.get_count_warnings(user_id) + 1
+            if set_warnings is not None:
+                count_warnings = set_warnings
+            return self.cursor.execute(
+                "UPDATE warnings SET count_warnings = (?) WHERE user_id = (?);", (count_warnings, user_id,)
+            )
+
+
+if __name__ == '__main__':
+    sqLighter = SQLighter('sql.db')
+    sqLighter.add_user(5)
+    sqLighter.add_new_warning(1)
