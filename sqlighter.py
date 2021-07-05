bbe339e97583e1eaae19f52f25a97800e90d95d0
counter0021 <ara123rus123ara@gmail.com>
2021-07-05 14:46:38 +0300

    Rename table

--- sqlighter.py
+++ sqlighter.py
@@ -10,27 +10,33 @@ class SQLighter:
     def __init__(self, database):
         self.connection = sqlite3.connect(database)
         self.cursor = self.connection.cursor()
-        self.create_table()
 
-    def create_table(self):
+
+class UserTable(SQLighter):
+
+    def __init__(self, database):
+        super().__init__(database)
+        self.create_users_table()
+
+    def create_users_table(self):
         table = """
-                    CREATE TABLE IF NOT EXISTS warnings (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        user_id VARCHAR(255) NOT NULL UNIQUE,
-                        count_warnings INTEGER NOT NULL DEFAULT 0
-                    );
-                """
+                        CREATE TABLE IF NOT EXISTS users (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            user_id VARCHAR(255) NOT NULL UNIQUE,
+                            count_warnings INTEGER NOT NULL DEFAULT 0
+                        );
+                    """
 
         with self.connection:
             self.cursor.execute(table)
-            logger.info('Table created')
+            logger.info('Table "users" created')
 
     def exists(self, user_id: int):
 
         with self.connection:
             user_exists = bool(
-                len(self.cursor.execute("SELECT * FROM warnings WHERE user_id = ?", (user_id,)).fetchall()))
-            logger.info(f"User exists = {user_exists}")
+                len(self.cursor.execute("SELECT * FROM users WHERE user_id = ?", (user_id,)).fetchall()))
+            logger.info(f'User exists = {user_exists}')
             return user_exists
 
     def add_user(self, user_id: int):
@@ -38,7 +44,7 @@ class SQLighter:
         with self.connection:
             logger.info(f'Add new user ({user_id})')
             if not self.exists(user_id):
-                return self.cursor.execute("INSERT INTO warnings (user_id) VALUES (?);", (user_id,))
+                return self.cursor.execute("INSERT INTO users (user_id) VALUES (?);", (user_id,))
             return
 
     def get_count_warnings(self, user_id: int):
@@ -46,7 +52,7 @@ class SQLighter:
         with self.connection:
             return int(
                 self.cursor.execute(
-                    "SELECT count_warnings FROM warnings WHERE user_id = (?);", (user_id,)
+                    "SELECT count_warnings FROM users WHERE user_id = (?);", (user_id,)
                 ).fetchone()[0]
             )
 
@@ -58,11 +64,11 @@ class SQLighter:
             if set_warnings is not None:
                 count_warnings = set_warnings
             return self.cursor.execute(
-                "UPDATE warnings SET count_warnings = (?) WHERE user_id = (?);", (count_warnings, user_id,)
+                "UPDATE users SET count_warnings = (?) WHERE user_id = (?);", (count_warnings, user_id,)
             )
 
 
 if __name__ == '__main__':
-    sqLighter = SQLighter('sql.db')
+    sqLighter = UserTable('sql.db')
     sqLighter.add_user(5)
-    sqLighter.add_new_warning(1)
+    sqLighter.add_new_warning(5)
