a59c0c3a5c82590bd97d158ec57570d2b372aa00
counter0021 <ara123rus123ara@gmail.com>
2021-07-05 19:51:40 +0300

    Help, info and docs

--- bot/bot.py
+++ bot/bot.py
@@ -22,16 +22,36 @@ KICK_MESSAGE_ID: int = 0
 
 
 # rules, help
+@dispatcher.message_handler(commands=['help'])
+async def help_bot(message: types.Message):
+    """ Help """
+    await message.answer(
+        'My commands:\n'
+        '\t\t\t\t<strong>/info</strong> - Information about me\n'
+        '\t\t\t\t<strong>/link</strong> - Invite link\n'
+        '\t\t\t\t<strong>/kick</strong> - Vote for kick. Need reply message\n'
+        '\t\t\t\t<strong>/chat</strong> - Id chat - only admin\n'
+        '\t\t\t\t<strong>/warning</strong> - Need reply message. Warning - only admin\n'
+        '\t\t\t\t<strong>/ban</strong> - Need reply message. Ban - only admin'
+    )
+
+
+@dispatcher.message_handler(commands=['info'])
+async def info(message: types.Message):
+    """ Information about bot """
+    await message.answer('I\'m <strong>Bot Moderator by _Counter021_!</strong>\nInput /help for detail information')
+
 
 @dispatcher.message_handler(commands=['chat'], is_admin=True)
 async def get_chat_id(message: types.Message):
+    """ Get chat ID """
     assert message.chat.id != GROUP_ID, 'GROUP_ID != chat.id'
     await message.answer(f'Chat id = {message.chat.id}')
 
 
-# Vote for kick
 @dispatcher.message_handler(commands=['kick'])
 async def vote_for_kick(message: types.Message):
+    """ Vote for kick """
     global KICK_MESSAGE_ID
 
     if KICK_MESSAGE_ID:
@@ -73,8 +93,86 @@ async def vote_for_kick(message: types.Message):
     db_vote.create_new_vote(user_id=user, message_id=KICK_MESSAGE_ID, kick_id=kick)
 
 
+@dispatcher.message_handler(commands=['warning'], is_admin=True)
+async def add_new_warning(message: types.Message):
+    """ New warning """
+
+    if not message.reply_to_message:
+        await message.reply('Command need use with reply message!')
+        return
+
+    user_may_be_is_admin = await message.bot.get_chat_member(GROUP_ID, message.reply_to_message.from_user.id)
+    if user_may_be_is_admin.is_chat_admin():
+        await message.reply('Error give warning admin!')
+        return
+
+    user = message.reply_to_message.from_user
+    user_id = user.id
+    db_user.add_new_warning(user_id)
+    count_warning = db_user.get_count_warnings(user_id)
+    await message.bot.delete_message(GROUP_ID, message.message_id)
+
+    if count_warning >= 3:
+        await bot.send_message(GROUP_ID, f'We warned you! @{user.username}')
+        await message.bot.kick_chat_member(GROUP_ID, user_id, revoke_messages=True)
+        await bot.send_message(GROUP_ID, f'User @{user.username} has been banned!')
+        await message.bot.send_photo(message.chat.id, 'https://airsofter.world/galleries/3857/5cb720e8783a1.jpg')
+    else:
+        await bot.send_message(
+            GROUP_ID, f'You have {count_warning} warnings! If you have 3 warnings we ban you! @{user.username}',
+        )
+
+
+@dispatcher.message_handler(commands=['link'])
+async def get_add_link(message: types.Message):
+    """ New invite link """
+    link = await message.bot.create_chat_invite_link(message.chat.id)
+    await message.answer(link['invite_link'])
+
+
+@dispatcher.message_handler(is_admin=True, commands=['ban'])
+async def ban(message: types.Message):
+    """ Ban """
+    if not message.reply_to_message:
+        await message.reply('Command need use with reply message!')
+        return
+
+    user = await message.bot.get_chat_member(GROUP_ID, message.reply_to_message.from_user.id)
+    if user.is_chat_admin():
+        await message.reply('Error ban admin!')
+        return
+
+    await message.bot.delete_message(GROUP_ID, message.message_id)
+    await message.bot.kick_chat_member(GROUP_ID, message.reply_to_message.from_user.id, revoke_messages=True)
+
+    await message.reply_to_message.reply(f'User @{message.reply_to_message.from_user.username} has been banned!')
+    await message.bot.send_photo(message.chat.id, 'https://airsofter.world/galleries/3857/5cb720e8783a1.jpg')
+
+
+@dispatcher.message_handler(
+    content_types=['new_chat_members', 'left_chat_member', 'new_chat_title', 'new_chat_photo']
+)
+async def on_user_joined(message: types.Message):
+    """ Delete service messages """
+    if message.content_type == 'new_chat_members':
+        for member in message.new_chat_members:
+            db_user.add_user(member['id'])
+            db_user.add_new_warning(member['id'], 0)
+            assert db_user.get_count_warnings(member.id) == 0, 'Member warning not is 0'
+    await message.delete()
+
+
+@dispatcher.message_handler()
+async def filter_message_with_link(message: types.Message):
+    """ Delete messages with types ['url', 'text_link', 'email', 'phone_number'] """
+    for entity in message.entities:
+        if entity.type in ['url', 'text_link', 'email', 'phone_number']:
+            await message.delete()
+
+
 @dispatcher.callback_query_handler(lambda callback_data: callback_data.data)
 async def callback(callback_query: types.CallbackQuery):
+    """ Treatment callback data """
     global KICK_MESSAGE_ID
 
     await bot.answer_callback_query(callback_query.id)
@@ -115,83 +213,6 @@ async def callback(callback_query: types.CallbackQuery):
             )
             await bot.send_message(GROUP_ID, 'You lucky man!')
             KICK_MESSAGE_ID = 0
-
-
-# New warning
-@dispatcher.message_handler(commands=['warning'], is_admin=True)
-async def add_new_warning(message: types.Message):
-
-    if not message.reply_to_message:
-        await message.reply('Command need use with reply message!')
-        return
-
-    user_may_be_is_admin = await message.bot.get_chat_member(GROUP_ID, message.reply_to_message.from_user.id)
-    if user_may_be_is_admin.is_chat_admin():
-        await message.reply('Error give warning admin!')
-        return
-
-    user = message.reply_to_message.from_user
-    user_id = user.id
-    db_user.add_new_warning(user_id)
-    count_warning = db_user.get_count_warnings(user_id)
-    await message.bot.delete_message(GROUP_ID, message.message_id)
-
-    if count_warning >= 3:
-        await bot.send_message(GROUP_ID, f'We warned you! @{user.username}')
-        await message.bot.kick_chat_member(GROUP_ID, user_id, revoke_messages=True)
-        await bot.send_message(GROUP_ID, f'User @{user.username} has been banned!')
-        await message.bot.send_photo(message.chat.id, 'https://airsofter.world/galleries/3857/5cb720e8783a1.jpg')
-    else:
-        await bot.send_message(
-            GROUP_ID, f'You have {count_warning} warnings! If you have 3 warnings we ban you! @{user.username}',
-        )
-
-
-# New add link
-@dispatcher.message_handler(commands=['link'])
-async def get_add_link(message: types.Message):
-    link = await message.bot.create_chat_invite_link(message.chat.id)
-    await message.answer(link['invite_link'])
-
-
-# Ban
-@dispatcher.message_handler(is_admin=True, commands=['ban'])
-async def ban(message: types.Message):
-    if not message.reply_to_message:
-        await message.reply('Command need use with reply message!')
-        return
-
-    user = await message.bot.get_chat_member(GROUP_ID, message.reply_to_message.from_user.id)
-    if user.is_chat_admin():
-        await message.reply('Error ban admin!')
-        return
-
-    await message.bot.delete_message(GROUP_ID, message.message_id)
-    await message.bot.kick_chat_member(GROUP_ID, message.reply_to_message.from_user.id, revoke_messages=True)
-
-    await message.reply_to_message.reply(f'User @{message.reply_to_message.from_user.username} has been banned!')
-    await message.bot.send_photo(message.chat.id, 'https://airsofter.world/galleries/3857/5cb720e8783a1.jpg')
-
-
-# Delete service messages
-@dispatcher.message_handler(
-    content_types=['new_chat_members', 'left_chat_member', 'new_chat_title', 'new_chat_photo']
-)
-async def on_user_joined(message: types.Message):
-    if message.content_type == 'new_chat_members':
-        for member in message.new_chat_members:
-            db_user.add_user(member['id'])
-            db_user.add_new_warning(member['id'], 0)
-            assert db_user.get_count_warnings(member.id) == 0, 'Member warning not is 0'
-    await message.delete()
-
-
-# Delete messages with types ['url', 'text_link', 'email', 'phone_number']
-@dispatcher.message_handler()
-async def filter_message_with_link(message: types.Message):
-    for entity in message.entities:
-        if entity.type in ['url', 'text_link', 'email', 'phone_number']:
-            await message.delete()
 
 
 if __name__ == '__main__':
